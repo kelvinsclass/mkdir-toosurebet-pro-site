2fcf30a6ebf1fc6a37b08aff5a1cad48e39c2b94
kelvinsclass <kelvinsclassdm@gmail.com>
2026-02-17 20:15:19 +0000

    Create Footer.js

---
+++ components/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-primary-green text-white p-4 mt-10">
+      <div className="max-w-7xl mx-auto text-center">
+        <p>&copy; 2026 TOOSUREBET. Tous droits réservés. Site professionnel pour paris sportifs.</p>
+      </div>
+    </footer>
+  );
+}
